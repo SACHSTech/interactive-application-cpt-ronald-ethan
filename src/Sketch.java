9bc2f2f1407a5028bce5dd16c0d4fe148d144f4d
ethan23g <ethan.gu28@ycdsbk12.ca>
2026-05-28 14:04:46 -0400

    add pen sketch that follows mouse

--- src/Sketch.java
+++ src/Sketch.java
@@ -12,6 +12,7 @@ public class Sketch extends PApplet {
     @Override
     public void settings() {
         size(600, 400); 
+        
     }
 
     @Override
@@ -21,7 +22,24 @@ public class Sketch extends PApplet {
 
     @Override
     public void draw() {
+        background(255);
+        drawNotebook();
+        drawPen();
+    }
 
+    private void drawNotebook() {
+        fill(150, 75, 0); // Brown notebook cover
+        rect(60, 40, 450, 320);
+        fill(255); // White paper 
+        rect(82, 50, 200, 300);
+        rect(288, 50, 200, 300);
+
+    }
+
+    private void drawPen() {
+        strokeWeight(10);
+        line(mouseX, mouseY, mouseX + 80, mouseY - 120);
+        strokeWeight(0);
     }
 
     /** Additional helper methods below */
