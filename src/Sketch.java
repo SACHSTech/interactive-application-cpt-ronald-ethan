b46605d0e474c6d378e8e63ec1329549525519c3
ethan23g <ethan.gu28@ycdsbk12.ca>
2026-06-03 13:39:33 -0400

    Add UI for shape selecting

--- src/Sketch.java
+++ src/Sketch.java
@@ -24,6 +24,7 @@ public class Sketch extends PApplet {
     public void draw() {
         background(255);
         drawNotebook(175, 75, 450, 320);
+        shapesUI();
         drawPen();
     }
 
@@ -49,7 +50,7 @@ public class Sketch extends PApplet {
         // Red Line on the notebook paper
         stroke(255, 180, 180);
         line(210, 75, 210, 75 + paperHeight);
-
+        line(210 + paperWidth / 2, 75, 210 + paperWidth / 2, 75 + paperHeight);
         // Notebook spine
         stroke(100, 50, 0);
         strokeWeight(3);
@@ -61,20 +62,43 @@ public class Sketch extends PApplet {
     }
 
     private void drawPen() {
-        // pen tip
+        // Pen tip
         strokeWeight(8);
         stroke(156, 153, 152);
         line(mouseX, mouseY, mouseX + 80, mouseY - 120);
-        // pen body
+        // Pen body
         strokeWeight(15);
         stroke(0);
         line(mouseX + 7, mouseY - 10, mouseX + 80, mouseY - 120);
-        // pen clip
+        // Pen clip
         stroke(3);
         line(mouseX + 54, mouseY - 90, mouseX + 70, mouseY - 120);
         strokeWeight(0);
     }
+    
+    private void shapesUI() {
 
+        // Boxes
+        strokeWeight(2);
+        int distance = 125;
+        for (int i = 0; i < 4; i++) {
+            rect(170 + distance * i, 420, 80, 80, 10);
+        }
+        stroke(0); 
+        fill(128, 128, 128);
+        // Rectangle
+        rect(180, 440, 60, 40);
+        
+        // Square
+        square(185 + distance, 435, 50);
+
+        // Circle
+        circle(210 + 2 * distance, 460, 55);
+
+        // Triangle
+        triangle(585, 435, 620, 485, 550, 485);
+    }
+    
    
 
     /** Additional helper methods below */
